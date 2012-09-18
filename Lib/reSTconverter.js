9415669cf5c86e5ae7be79c5971500ae67d1fa4f
goingmywaynet <joey-dm@goingmyway.net>
2012-09-18 22:24:09 +0900

    - new object "reSTconverter" - new Unit Test Tool "winJSUnitTest.js"

---
+++ Lib/reSTconverter.js
@@ -0,0 +1,67 @@
+﻿/* reSTconverter.js
+ * Version 0.1
+ * Author : Joey Chen
+ * Date   : 2012/09/17
+ *
+ * Description : 
+ *    string configure and some markup parse object for sphinx reST
+ *
+ * Update Log :
+ *
+ */
+
+//
+// reSTconverter Object
+//
+function reSTconverter(srcDirPath) {
+
+  //
+  // Property
+  //
+  this.indexFileName = "index.rst";   // index.rst file name
+  this.srcDirPath = srcDirPath;       // sphinx source files dir path
+
+  //
+  // Setter
+  //
+  this.setIndexFileName = function(name){
+    if (name) { this.indexFileName = name; 
+    } else {
+      this.indexFileName = "index.rst";
+    }
+  },
+
+  //
+  // Method : newIndexFile(filePath,pageTitle,addDate)
+  //
+  this.newIndexFile = function(filePath,pageTitle,addDate) {
+
+    var _objFileSys = WScript.CreateObject("Scripting.FileSystemObject");	// Create File System Object
+    var filePath = _objFileSys.BuildPath(filePath,this.indexFileName);
+
+    // Default Configure
+    var pageTitle = pageTitle ? pageTitle : "my page";
+    var addDate = addDate ? addDate : false;
+
+    var headerText = "\n=========================================\n" + pageTitle + "\n=========================================\n\n";
+
+    // addDate
+    if (addDate) {
+      var dt = new Date();
+      var dateString = "(" + parseInt(dt.getMonth()+1) + "/" + dt.getDate() + " " + dt.getHours() +":"+ dt.getMinutes() + " 生成) \n\n";
+      headerText = headerText + dateString;
+    }
+
+    try {
+      ADOStream_Save(filePath, headerText , 'utf-8');
+    } catch (result) {
+      MessageWindow_warn("Error at reSTconverter_openNewIndexFile", "ADOStream Save Error " + result + "\n filePath : " + filePath + "\n headerText : " + headerText , 0)
+      return null;
+    } 
+
+    return filePath;
+  }
+    
+}
+
+
---
+++ Lib/winJSUnitTest.js
@@ -0,0 +1,70 @@
+﻿/* winJSUniteTest.js
+ * Version 0.1
+ * Author  : Joey Chen
+ * Date    : 2012/09/17
+ *
+ * Description :
+ *
+ *   This is Windows JScript Unit test object
+ *
+ *   New Test Object
+ *    obj = new winJSUnitTest("Test Name");
+ *
+ *   New assert
+ *    obj.assert("check title", BOOL , "error message");
+ *
+ *   Show asserts summary
+ *    obj.showResult(secondsToCloseResultWindow);
+ *
+ *   End Test
+ *    obj.done();
+ *
+ * Update Log  :
+ *
+ */
+
+//
+// class winJSUnitTest(testName)
+//
+function winJSUnitTest(testName) {
+
+  //
+  // Property
+  //
+  this.testName = testName;
+  this.result = new Array();
+
+  //
+  // Method : assert
+  //
+  this.assert = function (title, exp, message){
+    if (!exp) {
+      // false assert
+      this.result.push("\u2716 " + (title?title:"something") + " ...\t" + (message?message:""));
+        // \u2716 is unicode "x"
+    } else {
+      // pass assert
+      this.result.push("\u2714 " + (title?title:"something") );
+        // \u2714 is unicode "check"
+    }
+  },
+
+  //
+  // Method : showResult
+  //
+  this.showResult = function(secondsToWait) {
+    var WshShell = WScript.CreateObject("WScript.Shell");
+    WshShell.Popup(this.result.join("\n") , secondsToWait , this.testName , 64);
+    WshShell = null;
+  },
+
+
+  //
+  // Method : done
+  //
+  this.done = function() {
+    this.showResult();
+    this.result = null;
+  }
+
+}
